eaf6b1699c2e281a8f60b56a73bc73fb83cedad1
blak-kong <546598185@qq.com>
2019-11-04 08:57:21 +0800

    升级到gulp4,追加热更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 // 引入gulp和gulp插件
 var gulp = require('gulp'),
-    runSequence = require('run-sequence'),               // gulp任务运行顺序
     rev = require('gulp-rev'),                           // 生成版本号映射
     revCollector = require('gulp-rev-collector'),        // 添加版本号
     jshint = require('gulp-jshint'),		 		     // js语法检查
@@ -11,12 +10,13 @@ var gulp = require('gulp'),
     csso = require('gulp-csso'),						 // 合并css属性
 	csslint = require('gulp-csslint'),					 // css语法检查
 	autoprefixer = require('gulp-autoprefixer'),		 // 添加浏览器前缀
-	csscomb = require('gulp-csscomb'),					 // css 样式表的各属性的顺序
 	cache = require('gulp-cache'),						 // 缓存处理
 	replace = require('gulp-replace'),					 // 替换路径
     clean = require('gulp-clean'),						 // 清理文目标文件夹
     babel = require("gulp-babel"),
-    requirejsOptimize = require('gulp-requirejs-optimize') // require打包压缩(引入模块打包到js，减少引入)
+    requirejsOptimize = require('gulp-requirejs-optimize'), // require打包压缩(引入模块打包到js，减少引入)
+    connect = require('gulp-connect'), // 热更新
+    concat = require("gulp-concat") // 合并js
 
     // gulp-rev-append
 
@@ -29,7 +29,7 @@ var cssSrc = 'css/*.css',
 
 // CSS生成文件hash编码并生成 rev-manifest.json文件名对照映射 
 gulp.task('revCss', function() {
-    return gulp.src(cssSrc)
+     gulp.src(cssSrc)
         .pipe(rev())
         .pipe(rev.manifest())
         .pipe(gulp.dest('rev/css'));
@@ -37,7 +37,7 @@ gulp.task('revCss', function() {
 
 //js生成文件hash编码并生成 rev-manifest.json文件名对照映射 
 gulp.task('revJs', function() {
-    return gulp.src(jsSrc)
+     gulp.src(jsSrc)
         .pipe(rev())
         .pipe(rev.manifest())
         .pipe(gulp.dest('rev/js'));
@@ -48,9 +48,11 @@ gulp.task('revJs', function() {
 // 在命令行使用 gulp csscompress 启动此任务
 gulp.task('csscompress', function() {
     // 1. 找到文件
-  return  gulp.src('css/*.css')
+    gulp.src('css/*.css')
         // 2. 压缩文件
         .pipe(cssmin())
+        .pipe(autoprefixer())
+        .pipe(csslint())
         // 3. 另存压缩后的文件
         .pipe(gulp.dest('dist/css'));
 })
@@ -59,7 +61,7 @@ gulp.task('csscompress', function() {
 // 在命令行使用 gulp script 启动此任务
 gulp.task('jscompress', function() {
     // 1. 找到文件
-   return gulp.src('js/*.js')
+    gulp.src('js/*.js')
         // 3. 运行babel(注意，babel只转换语法，不转换api)
         .pipe(babel())
         // 3. 另存压缩后的文件
@@ -70,7 +72,7 @@ gulp.task('jscompress', function() {
 
 // 合并js
 gulp.task("requirejs", function() {
-    return gulp.src("js/*.js")
+     gulp.src("js/*.js")
             
            .pipe(
                 requirejsOptimize({
@@ -83,19 +85,20 @@ gulp.task("requirejs", function() {
 
 // 压缩图片
 gulp.task('imageMin', function () {
-    return gulp.src('images/*.{png,jpg}')
-        // .pipe(imagemin({
-        //     optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
-        //     progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
-        //     interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
-        //     multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
-        // }))
+     gulp.src('images/*.{png,jpg}')
+        .pipe(cache(imageMin({
+                optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+                progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+                interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+                multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+            })
+        ))
         .pipe(gulp.dest('dist/images'));
 });
 
 // 压缩icon
 gulp.task('icon', function () {
-    return gulp.src('icon/*.{png,jpg}')
+     gulp.src('icon/*.{png,jpg}')
         // 不作处理 直接复制黏贴
         .pipe(gulp.dest('dist/icon'));
 });
@@ -134,7 +137,7 @@ gulp.task('revHtml', function() {
         minifyJS: true,//压缩页面JS
         minifyCSS: true//压缩页面CSS
     };
-  return gulp.src(['rev/**/*.json', htmlSrc]) // 这里的View/*.html是项目html文件路径，如果gulpfile.js和html文件同在一级目录下，修改为return;
+ gulp.src(['rev/**/*.json', htmlSrc]) // 这里的View/*.html是项目html文件路径，如果gulpfile.js和html文件同在一级目录下，修改为return;
     .pipe(revCollector())
     .pipe(htmlmin(options)) // html压缩
     .pipe(gulp.dest('dist/pages/'));// 注意这里是生成的新的html文件，如果设置为你的项目html文件所在文件夹，会覆盖旧的html文件，若上面的template/*.html修改了，这里也要相应修改;----不能把源文件覆盖！否则会出现bug，源文件不断被替换，版本号越来越多！
@@ -152,7 +155,7 @@ gulp.task('revRootHtml', function() {
         minifyJS: true,//压缩页面JS
         minifyCSS: true//压缩页面CSS 
     };
-  return gulp.src(['rev/{css,img,js}/*.json', 'src/*.html']) // 这里的View/*.html是项目html文件路径，如果gulpfile.js和html文件同在一级目录下，修改为return;
+   gulp.src(['rev/{css,img,js}/*.json', 'src/*.html']) // 这里的View/*.html是项目html文件路径，如果gulpfile.js和html文件同在一级目录下，修改为return;
     .pipe(revCollector())
     .pipe(htmlmin(options)) // html压缩 
     .pipe(gulp.dest('dist/'));
@@ -161,10 +164,16 @@ gulp.task('revRootHtml', function() {
 
 //监控文件变化
 gulp.task('watch', function() {
-    gulp.watch([cssSrc, jsSrc, htmlSrc, 'js/*.js', '*.html'], ['dev']);
+    gulp.watch([cssSrc, jsSrc, htmlSrc, 'js/*.js', '*.html']);
 });
 
-//开发构建
-gulp.task('dev', function(cb) {
-    runSequence(['revCss'], ['revJs'],['requirejs'],['csscompress'],['jscompress'],['imageMin'],['icon'],['revHtml'],['revRootHtml'],['replaceURL'],['watch'], cb);
-});
+gulp.task('connect',function(){
+    connect.server({
+        root: 'dist',//根目录
+        // ip:'192.168.11.62',//默认localhost:8080
+        livereload: true,//自动更新
+        port: 8089
+    })
+})
+
+gulp.task("dev", gulp.series(gulp.parallel('connect','revCss', 'revJs','requirejs','csscompress','jscompress','imageMin','icon','revHtml','revRootHtml','replaceURL','watch')))
